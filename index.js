56d6b15ca4a27e1d16c48d9929f382898b05b2f1
shivanshi <73399160+shivanshi-s@users.noreply.github.com>
2022-05-20 00:01:29 +0530

    Day 3 JS files

---
+++ index.js
@@ -0,0 +1,73 @@
+// // console.log('Hello World!!!');
+
+// // const name = 'shivanshi';
+// // console.log(name);
+// // name = 'ram';
+// // -----------------------
+// //  data types
+
+// let name = 'shivanshi';
+// let marks = 90;
+// let marks2 = -4 ; 
+// console.log(typeof(name))
+// console.log(typeof(marks))
+// console.log(typeof(marks2))
+
+// let exams = true;
+// console.log(typeof(exams));
+
+// // undefined
+// let bowl;
+// console.log(typeof(bowl));
+
+// let water = null ;
+// console.log(typeof(water));
+
+
+// referennce types
+// let arr = ['red', 1 , true, '😀']
+
+// console.log(arr);
+// console.log(arr.length)
+
+// objects
+// const person = { 
+//    firstName : 'John', 
+//    lastName : 'Doe', 
+//    age : 14 
+//    }; 
+//    console.log(person) ;
+//    console.log(person.age);
+//    console.log(person.firstName +" " + person.lastName);
+
+// function greet() {
+//    console.log("This is a function");
+//    }
+// greet();
+
+// parameters - numberOne
+   // function add(numberOne , numberTwo) {
+
+   //    let sum = numberOne + numberTwo;
+      
+   //    return sum;
+      
+   //    }
+   //    // passing arguments
+   //    let result = add(3 , 4);
+      
+   //    console.log(result);
+
+
+
+   // for (let i = 0 ; i <= 10 ; i++)
+   // {
+   //  console.log(i)
+   //  }
+
+    let i = 0;
+    do {
+    i += 1;
+    console.log(i);
+    } while (i < 5);
+
---
+++ task.js
@@ -0,0 +1,10 @@
+let table = 2;
+let count = 10;
+
+console.log(myTable(table,count));
+
+function myTable(a, b){
+   for(let i=1;i<=b;i++){
+      console.log(i*a);
+   }
+}
